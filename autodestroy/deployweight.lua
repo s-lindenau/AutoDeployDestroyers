65e7d94531d07bd5359f5ea6ed0dc1a0e34cc190
Jon Atkins <github@jonatkins.com>
2016-09-29 00:06:52 +0100

    calculate weight, rather than useing a lookup table
    weight/300 chosen after testing in game over a range of evolution valuea
    and enemy base sizes

--- autodestroy/deployweight.lua
+++ autodestroy/deployweight.lua
@@ -3,39 +3,12 @@
 -- https://forums.factorio.com/memberlist.php?mode=viewprofile&u=16823
 -- Licence: GPLv3
 
--- Provides the target count to deploy if the weight is between {previous} and {current}.
--- Deploy amount is always capped to the 'max_follower_count'
---
--- So for example:
--- The lowest entry in the config describes the deploy amount for weights between 0 and {weight}
--- The second entry in the config is valid for weights between {previous weight} and {weight}
--- Anything above the biggest entry will deploy the 'max_follower_count' (134 in vanilla)
-local deployWeightConfig = {
-    { weight = 100, deployCount = 5 },
-    { weight = 1000, deployCount = 10 },
-    { weight = 5000, deployCount = 50 },
-    { weight = 10000, deployCount = 100 },
-    { weight = 12500, deployCount = 125 }
-}
-
 function getDeployCountForWeight(weight, max_follower_count)
     if (weight == nil) then
         return 0;
     end
 
-    table.sort(deployWeightConfig, deployWeightConfigComparator);
-    for _, deployConfig in pairs(deployWeightConfig) do
-        local deployWeight = deployConfig.weight;
-        local deployCount = deployConfig.deployCount;
-
-        if (weight < deployWeight) then
-            return math.min(deployCount, max_follower_count);
-        end
-    end
+    local deployCount = math.ceil(weight/300.0)
 
     return max_follower_count;
-end
-
-function deployWeightConfigComparator(first, second)
-    return first.weight < second.weight;
 end
