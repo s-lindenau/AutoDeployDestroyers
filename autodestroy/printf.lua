f43602556d03bcf39cabbb0e2a3f40586d15764a
slindenau <slindenau@home.nl>
2026-04-27 10:52:48 +0200

    Update docstrings in printf.lua

--- autodestroy/printf.lua
+++ autodestroy/printf.lua
@@ -1,7 +1,13 @@
--- Print a formatted string to the player console
+-- Author: slindenau
+-- https://github.com/s-lindenau
+-- https://forums.factorio.com/memberlist.php?mode=viewprofile&u=16823
+-- Licence: GPLv3
+
+--- Print a formatted string to the player console
 ---@param player LuaPlayer
 ---@param message_template string
----@vararg any # Message parameters to replace in the message_template
+---@param ... any # Message parameters to replace in the message_template
+---@diagnostic disable-next-line: unused
 function printf(player, message_template, ...)
     local message = string.format(message_template, ...);
     player.print(message);
